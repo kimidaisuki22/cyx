90269d0829672df9ab8c4895bff1603b73eb3027
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2024-04-20 20:20:20 +0800

    enable log in cyx.

--- cmakes/libs.cmake
+++ cmakes/libs.cmake
@@ -1,2 +1,5 @@
 find_package(zstd CONFIG REQUIRED)
 target_link_libraries(cyx PRIVATE $<IF:$<TARGET_EXISTS:zstd::libzstd_shared>,zstd::libzstd_shared,zstd::libzstd_static>)
+
+find_package(spdlog CONFIG REQUIRED)
+target_link_libraries(cyx PRIVATE spdlog::spdlog)
